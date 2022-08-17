ac8b52148f226f344f23c374c5a4906d64e724b7
Kehinde <you@example.com>
2022-08-17 14:29:28 -0400

    version2

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 #Deploying a python web application
-echo "Hello World"
+#echo "Hello World"
 #= bash shell
-python ('Hello World') 
+#python ('Hello World') 
 #=python scrpt
 
+print('Hello World')
